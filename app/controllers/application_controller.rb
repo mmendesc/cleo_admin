f3bc1193e20a3714ee43fa51f0dc8b6e415ffbde
mmendesc <matheus.mendescf@gmail.com>
2017-09-25 21:58:17 -0300

    Fix saloon for master employee, sign_out path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,15 @@ class ApplicationController < ActionController::Base
   #protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    case resource
-    when Employee
+    if resource.role == 'master'
+      admin_saloons_path
+    else
       admin_calendar_path
     end
+  end
+
+  def after_sign_out_path_for(resource)
+    new_employee_session_path
   end
 
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -6,7 +6,7 @@ class Employee < ApplicationRecord
 
   has_one :contract, inverse_of: :employee
   has_many :abilities, inverse_of: :employee
-  belongs_to :saloon
+  belongs_to :saloon, optional: true
   #belongs_to :responsible, class_name: 'Saloon' ,foreign_key: 'manager_id'
 
   delegate :name, to: :saloon, prefix: :true, allow_nil: true
@@ -14,9 +14,19 @@ class Employee < ApplicationRecord
   accepts_nested_attributes_for :contract
   accepts_nested_attributes_for :abilities
 
-  enum role: {worker: 0, manager: 1,master: 3}
+  enum role: {worker: 0, manager: 1,master: 2}
   enum gender: {male: 0, female: 1}
 
   scope :by_saloon, ->(saloon) {where(saloon_id: saloon)}
   scope :not_master, -> {where.not(role: 3)}
+
+  validate :saloon_presence
+
+  def saloon_presence
+    errors.add(:saloon_id, 'Salão é necessário.') if need_saloon
+  end
+
+  def need_saloon
+    self.role != 'master' && self.saloon_id.nil?
+  end
 end
