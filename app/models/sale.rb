b196bfd5c203a3ae11d435478dfe25ea9d13b319
mmendesc <matheus.mendescf@gmail.com>
2017-11-07 18:04:54 -0300

    Moved report logic to saloon model

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -4,54 +4,12 @@ class Sale < ApplicationRecord
   belongs_to :employee
   accepts_nested_attributes_for :items
 
+  scope :by_saloon, ->(saloon) { joins(:employee).where(employees: {saloon_id: saloon})}
 
   def total_price
     total = 0
     self.items.each do |item|
       total = total + item.total_price
-    end
-    total
-  end
-
-
-  def self.top_ten
-    expenses = []
-    Client.all.each do |client|
-      expenses << [client.total_expense,client.id]
-    end
-    expenses.sort.last(10)
-  end
-
-  def self.total_revenue
-    total = 0
-    Sale.all.each do |sale|
-      total = total + sale.total_price
-    end
-    total
-  end
-
-  def self.product_revenue(name)
-    product = Product.find_by(name: name)
-    total = 0
-    Sale.joins(:items).where('items.product_id = ?',product.id).each do |sale|
-      total = total + sale.total_price
-    end
-    total
-  end
-
-  def self.revenue_by_day(date)
-    total = 0
-    Sale.where(created_at: date.beginning_of_day..date.end_of_day).each do |sale|
-      total = total + sale.total_price
-    end
-    total
-  end
-
-  def self.revenue_by_day_and_product(date,name)
-    product = Product.find_by(name: name)
-    total = 0
-    Sale.joins(:items).where("sales.created_at >= ? AND sales.created_at <= ? and items.product_id = ?", date.beginning_of_day,date.end_of_day,product.id).each do |sale|
-      total = total + sale.total_price
     end
     total
   end
--- app/models/saloon.rb
+++ app/models/saloon.rb
@@ -3,4 +3,49 @@ class Saloon < ApplicationRecord
   has_many :managers, class_name: 'Employee', foreign_key: 'manager_id', inverse_of: :saloon
 
   accepts_nested_attributes_for :managers
+
+
+  def top_ten
+    expenses = []
+    Client.by_saloon(self.id).all.each do |client|
+      expenses << [client.total_expense,client.id]
+    end
+    expenses.sort.last(10)
+  end
+
+  def total_revenue
+    total = 0
+    Sale.by_saloon(self.id).all.each do |sale|
+      total = total + sale.total_price
+    end
+    total
+  end
+
+  def product_revenue(name)
+    product = Product.find_by(name: name)
+    total = 0
+    Sale.by_saloon(self.id).joins(:items).where('items.product_id = ?',product.id).each do |sale|
+      total = total + sale.total_price
+    end
+    total
+  end
+
+
+  def revenue_by_day(date)
+    total = 0
+    Sale.by_saloon(self.id).where(created_at: date.beginning_of_day..date.end_of_day).each do |sale|
+      total = total + sale.total_price
+    end
+    total
+  end
+
+  def revenue_by_day_and_product(date,name)
+    product = Product.find_by(name: name)
+    total = 0
+    Sale.by_saloon(self.id).joins(:items).where("sales.created_at >= ? AND sales.created_at <= ? and items.product_id = ?", date.beginning_of_day,date.end_of_day,product.id).each do |sale|
+      total = total + sale.total_price
+    end
+    total
+  end
+
 end
